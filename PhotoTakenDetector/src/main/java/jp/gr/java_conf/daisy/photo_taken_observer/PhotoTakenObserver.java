f70fb998d843c1ba3437ce836bb6dd13a495dbe6
daisy1754 <d-part@hotmail.co.jp>
2014-04-08 11:38:55 +0900

    Ignore old image to avoid false positive caused by photo deletion or update

--- PhotoTakenDetector/src/main/java/jp/gr/java_conf/daisy/photo_taken_observer/PhotoTakenObserver.java
+++ PhotoTakenDetector/src/main/java/jp/gr/java_conf/daisy/photo_taken_observer/PhotoTakenObserver.java
@@ -8,6 +8,7 @@ import android.provider.MediaStore;
 
 
 public class PhotoTakenObserver extends ContentObserver {
+    private final long LAST_TAKEN_PHOTO_THRESHOLD_MILLIS = 1000 * 60;
     private final Context mContext;
     private final Uri mObservedUri;
 
@@ -25,15 +26,28 @@ public class PhotoTakenObserver extends ContentObserver {
     @Override
     public void onChange(boolean selfChange, Uri uri) {
         super.onChange(selfChange);
+
         Cursor cursor = mContext.getContentResolver().query(uri,
-                new String[] {MediaStore.MediaColumns.DATA, MediaStore.MediaColumns.MIME_TYPE},
+                new String[]{MediaStore.MediaColumns.DATA, MediaStore.MediaColumns.DATE_ADDED,
+                        MediaStore.MediaColumns.MIME_TYPE, MediaStore.Images.ImageColumns.DATE_TAKEN},
                 null,
                 null,
                 MediaStore.MediaColumns.DATE_ADDED + " DESC");
+
         cursor.moveToFirst();
+        int dateTakenIndex = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATE_TAKEN);
+        Long dateTaken = cursor.getLong(dateTakenIndex);
+        long dateAdded = cursor.getLong(cursor.getColumnIndex(MediaStore.MediaColumns.DATE_ADDED)) * 1000;
+        if (System.currentTimeMillis() - dateTaken > LAST_TAKEN_PHOTO_THRESHOLD_MILLIS) {
+            PhotoTakenLogger.debugLog("old photo, ignored");
+            cursor.close();
+            PhotoTakenLogger.debugLog("taken:" + dateTaken + " added:" + dateAdded);
+            return;
+        }
         String filePath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA));
         String mimeType = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.MIME_TYPE));
         cursor.close();
         PhotoTakenLogger.logPhotoTaken("Observer", filePath, mimeType);
+        PhotoTakenLogger.debugLog("taken:" + dateTaken + " added:" + dateAdded);
     }
 }
